c35a20ea3dae9e99493ac3637598bfa6410bcef2
Zubair Omer <omer.zubair01@gmail.com>
2021-06-03 19:25:56 -0700

    Error.js file updated

--- React Beach Resort Project/src/pages/Error.js
+++ React Beach Resort Project/src/pages/Error.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 
 function Error() {
     return (
